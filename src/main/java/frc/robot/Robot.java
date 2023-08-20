659c84af5848f054f4daf6858efd40e4916fabe2
DataRange <pyexClan@gmail.com>
2023-08-20 16:06:11 -0500

    caught error

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -114,7 +114,7 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    neo.set(control.calculate(encoder.getVelocity()) + feedforward.calculate(setPoint));
+    neo.setVoltage(control.calculate(encoder.getVelocity()) + feedforward.calculate(setPoint));
   }
 
   /** This function is called once when the robot is disabled. */
